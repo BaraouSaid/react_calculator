b2973dc175e03db8634f34e25757ecb2c44811fa
Said <slaoualy@yahoo.fr>
2023-08-23 06:40:29 +0200

    added some style in the display div

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ const App = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="bg-black w-96 rounded-lg my-10 p-5">
-        <h1 className="text-white mb-3">CALCULATOR</h1>
-        <div className="output bg-black h-20 p-2 border-2 border-b-stone-500 border-r-stone-500 border-stone-300">
-          <div className="previous-operand"></div>
-          <div className="current-operand"></div>
+        <h1 className="text-white mb-3 font-bold text-lg">CALCULATOR</h1>
+        <div className="output flex flex-col items-end justify-around break-all bg-black h-20 p-2 border-2 border-b-stone-500 border-r-stone-500 border-stone-300">
+          <div className="previous-operand text-white">0256</div>
+          <div className="current-operand text-white text-xl">23654</div>
         </div>
         <div className="grid grid-cols-4 gap-4 mt-5 font-bold text-lg">
           <button className="bg-red-500 p-1 w-30 h-20 cols-2 rounded-xl col-span-2">
