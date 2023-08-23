6024b705c1e4e146af831b2e35a6e5c6f95dc20a
Said <slaoualy@yahoo.fr>
2023-08-23 07:22:03 +0200

    added all the id's on the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { useReducer } from 'react';
 
+const ACTIONS = {
+  ADD_NUMBER: 'add-number',
+  CLEAR: 'clear',
+};
+
 const reducer = (state, action) => {};
 
 const App = () => {
@@ -13,35 +18,83 @@ const App = () => {
       <div className="bg-black w-96 rounded-lg my-10 p-5">
         <h1 className="text-white mb-3 font-bold text-lg">CALCULATOR</h1>
         <div className="output flex flex-col items-end justify-around break-all bg-black h-20 p-2 border-2 border-b-stone-500 border-r-stone-500 border-stone-300">
-          <div className="previous-operand text-white">{previousOperand}</div>
+          <div className="previous-operand text-white">
+            {previousOperand} {operation}
+          </div>
           <div className="current-operand text-white text-xl">
             {currentOperand}
           </div>
         </div>
         <div className="grid grid-cols-4 gap-4 mt-5 font-bold text-lg">
-          <button className="bg-red-500 p-1 w-30 h-20 cols-2 rounded-xl col-span-2">
+          <button
+            id="clear"
+            className="bg-red-500 p-1 w-30 h-20 cols-2 rounded-xl col-span-2"
+          >
             AC
           </button>
-          <button className="bg-stone-100 p-1 w-20 h-20 rounded-xl">/</button>
-          <button className="bg-stone-100 p-1 w-20 h-20 rounded-xl">X</button>
-          <button className="bg-stone-100 p-1 w-20 h-20 rounded-xl">7</button>
-          <button className="bg-stone-100 p-1 w-20 h-20 rounded-xl">8</button>
-          <button className="bg-stone-100 p-1 w-20 h-20 rounded-xl">9</button>
-          <button className="bg-stone-100 p-1 w-20 h-20 rounded-xl">-</button>
-          <button className="bg-stone-100 p-1 w-20 h-20 rounded-xl">4</button>
-          <button className="bg-stone-100 p-1 w-20 h-20 rounded-xl">5</button>
-          <button className="bg-stone-100 p-1 w-20 h-20 rounded-xl">6</button>
-          <button className="bg-stone-100 p-1 w-20 h-20 rounded-xl">+</button>
-          <button className="bg-stone-100 p-1 w-20 h-20 rounded-xl">1</button>
-          <button className="bg-stone-100 p-1 w-20 h-20 rounded-xl">2</button>
-          <button className="bg-stone-100 p-1 w-20 h-20 rounded-xl">3</button>
-          <button className="bg-blue-500 p-1 w-20 h-30 rounded-xl row-span-2">
+          <button id="divide" className="bg-stone-100 p-1 w-20 h-20 rounded-xl">
+            /
+          </button>
+          <button
+            id="multiply"
+            className="bg-stone-100 p-1 w-20 h-20 rounded-xl"
+          >
+            X
+          </button>
+          <button id="seven" className="bg-stone-100 p-1 w-20 h-20 rounded-xl">
+            7
+          </button>
+          <button id="eight" className="bg-stone-100 p-1 w-20 h-20 rounded-xl">
+            8
+          </button>
+          <button id="nine" className="bg-stone-100 p-1 w-20 h-20 rounded-xl">
+            9
+          </button>
+          <button
+            id="substract"
+            className="bg-stone-100 p-1 w-20 h-20 rounded-xl"
+          >
+            -
+          </button>
+          <button id="four" className="bg-stone-100 p-1 w-20 h-20 rounded-xl">
+            4
+          </button>
+          <button id="five" className="bg-stone-100 p-1 w-20 h-20 rounded-xl">
+            5
+          </button>
+          <button id="six" className="bg-stone-100 p-1 w-20 h-20 rounded-xl">
+            6
+          </button>
+          <button id="add" className="bg-stone-100 p-1 w-20 h-20 rounded-xl">
+            +
+          </button>
+          <button id="one" className="bg-stone-100 p-1 w-20 h-20 rounded-xl">
+            1
+          </button>
+          <button id="two" className="bg-stone-100 p-1 w-20 h-20 rounded-xl">
+            2
+          </button>
+          <button id="three" className="bg-stone-100 p-1 w-20 h-20 rounded-xl">
+            3
+          </button>
+          <button
+            id="equals"
+            className="bg-blue-500 p-1 w-20 h-30 rounded-xl row-span-2"
+          >
             =
           </button>
-          <button className="bg-stone-100 p-1 w-30 h-20 rounded-xl col-span-2">
+          <button
+            id="zero"
+            className="bg-stone-100 p-1 w-30 h-20 rounded-xl col-span-2"
+          >
             0
           </button>
-          <button className="bg-stone-100 p-1 w-20 h-20 rounded-xl">.</button>
+          <button
+            id="decimal"
+            className="bg-stone-100 p-1 w-20 h-20 rounded-xl"
+          >
+            .
+          </button>
         </div>
       </div>
     </div>
