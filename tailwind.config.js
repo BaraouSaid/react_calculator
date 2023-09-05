c152753054e0e3a7739f2cc5f58a35597437bbd5
Said <slaoualy@yahoo.fr>
2023-09-05 21:54:02 +0200

    set the Aldrich font family to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+
+    fontFamily: {
+      sans: ['ui-sans-serif', 'system-ui'],
+      serif: ['ui-serif', 'Georgia'],
+      mono: ['ui-monospace', 'SFMono-Regular'],
+      display: ['Oswald'],
+      body: ['"Aldrich"'],
+    },
   },
   plugins: [],
 };
